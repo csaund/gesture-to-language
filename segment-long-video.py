421dc25550da18fe21675f5d253d6b63420ea70c
Carolyn Saund <carolyn.saund@gmail.com>
2019-09-17 15:09:45 +0100

    added individual transcripts to indiv gestures

--- segment-long-video.py
+++ segment-long-video.py
@@ -4,6 +4,7 @@ import argparse
 import io
 import subprocess
 import json
+import copy
 
 # from moviepy.editor import VideoFileClip, AudioFileClip
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
@@ -17,6 +18,31 @@ collection = service.documents()
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/carolynsaund/google-creds.json"
 
+
+#   "phrases":
+#   [
+#       {
+#         "id": 1,
+#         "phase": {
+#             "start_seconds": 363,
+#             "end_seconds": 378.1
+#         },
+#         "gestures": [
+#             {
+#               "start_seconds": 364,
+#               "end_seconds": 365.5
+#             },
+#             {
+#               "start_seconds": 365.6,
+#               "end_seconds": 366
+#             },
+#         ]
+#     },
+#     ...
+# ]
+# writes out to filename_base/filename_base-id.json
+# with addition of words that match the gesture phrase.
+# gesture_clip_timings is the dict that we want to append to.
 def transcribe_files(filename_base, gesture_clip_timings):
     """Transcribe the given audio file synchronously and output the word time
     offsets."""
@@ -26,11 +52,14 @@ def transcribe_files(filename_base, gesture_clip_timings):
     client = speech.SpeechClient()
     dir_path = "./" + filename_base
 
-    for gesture_phase in gesture_clip_timings:
-        input_vid_path = dir_path + '/' + filename_base + '_' + str(gesture_phase['id']) + '.mp4'
-        output_audio_path = dir_path + '/' + filename_base + '_' + str(gesture_phase['id']) + '.wav'
-        output_transcript_path = dir_path + '/' + filename_base + '_' + str(gesture_phase['id']) + '.json'
-        # todo there is a better way of iterating thru all items in a dir probably
+    output_data = copy.deepcopy(gesture_clip_timings)
+
+    for gesture_phrase in output_data:
+        print
+        print "NEW GESTURE"
+        input_vid_path = dir_path + '/' + filename_base + '_' + str(gesture_phrase['id']) + '.mp4'
+        output_audio_path = dir_path + '/' + filename_base + '_' + str(gesture_phrase['id']) + '.wav'
+        # TODO there is a better way of iterating thru all items in a dir probably
 
         command = ("ffmpeg -i %s -ab 160k -ac 2 -ar 48000 -vn %s" % (input_vid_path, output_audio_path))
         subprocess.call(command, shell=True)
@@ -48,23 +77,39 @@ def transcribe_files(filename_base, gesture_clip_timings):
 
         response = client.recognize(config, audio)
 
-        fn = output_transcript_path
-        transcript = {}
-        with open(fn, 'w') as f:
-            for result in response.results:
-                alternative = result.alternatives[0]
-                transcript['Transcript'] = str(alternative.transcript)
+        for result in response.results:
+            alternative = result.alternatives[0]
+            gesture_phrase['phase']['transcript'] = str(alternative.transcript)
 
-                transcript['words'] = []
-                for i in range(len(alternative.words)):
-                    transcript['words'].append({})
-                    word_info = alternative.words[i]
-                    transcript['words'][i]['word'] = str(word_info.word)
-                    transcript['words'][i]['start_time'] = word_info.start_time.nanos *  1e-9
-                    transcript['words'][i]['end_time'] = word_info.end_time.nanos * 1e-9
+            ## go through and add specific transcripts to each gesture based on timings
+            gesture_index = 0
+            current_gesture = gesture_phrase['gestures'][0]
+            current_gesture['transcript'] = ""
+            gesture_phrase_start = gesture_phrase['phase']['start_seconds']
+            for i in range(len(alternative.words)):
+                word_info = alternative.words[i]
 
-            json.dump(dict(transcript), f, indent=4)
-        f.close()
+                ## nanos is super f'ed up in these responses? like they go up and down a bunch?
+                ## let's just try with seconds and see where we get.
+                # word_start = word_info.start_time.nanos *  1e-9
+                # word_end = word_info.start_time.nanos *  1e-9
+                word_start = word_info.start_time.seconds
+                word_end = word_info.start_time.seconds
+                # we're still in the same gesture phrase
+                if (word_start + gesture_phrase_start) < current_gesture['end_seconds']:
+                    current_gesture['transcript'] += " " + word_info.word
+                else:
+                    gesture_index += 1
+                    current_gesture = gesture_phrase['gestures'][gesture_index]
+                    current_gesture['transcript'] = word_info.word
+            ## only do this for first alternative.
+            break
+
+    output_transcript_path = dir_path + '/' + filename_base + '_transcripts' + '.json'
+    fn = output_transcript_path
+    with open(fn, 'w') as f:
+        json.dump(output_data, f, indent=4)
+    f.close()
 
 
 ## TODO calculate the timestamps automatically, or take them as an arg
@@ -76,13 +121,30 @@ def make_clip_timings(filename_base):
     return timings
 
 
-# get timings as list of dicts with shape:
-#        {
-#            id: 1,
-#            start_seconds: 367.5,
-#            end_seconds: 369.0
-#        }
+
 ## assumes video subfolder is already created
+# get timings as list of dicts with shape:
+#   "phrases":
+#   [
+#       {
+#         "id": 1,
+#         "phase": {
+#             "start_seconds": 363,
+#             "end_seconds": 378.1
+#         },
+#         "gestures": [
+#             {
+#               "start_seconds": 364,
+#               "end_seconds": 365.5
+#             },
+#             {
+#               "start_seconds": 365.6,
+#               "end_seconds": 366
+#             },
+#         ]
+#     },
+#     ...
+# ]
 def segment_video(filename_base, video_path, gesture_clip_timings):
     dir_path = "./" + filename_base
     for gesture_phase in gesture_clip_timings:
