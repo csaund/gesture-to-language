48a5a97a3df6b6ca0fc84c3814ebc75d783a9830
Carolyn Saund <carolyn.saund@gmail.com>
2019-09-24 16:12:02 +0100

    cleanup

--- cluster_sentences.py
+++ cluster_sentences.py
@@ -1,25 +1,28 @@
 #!/usr/bin/env python
 print "loading modules"
+
+## Standard Fare
 import argparse
-import nltk
-from gensim.models import Word2Vec
 import json
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn import cluster
-from sklearn import metrics
-from sklearn.cluster import KMeans
-from nltk.cluster import KMeansClusterer
 import numpy as np
 import re
 import string
 import matplotlib.pyplot as plt
+
+## Word Stuff
+import nltk
+from nltk.cluster import KMeansClusterer
+from gensim.models import Word2Vec
+
+## SKLEARN
+from sklearn import cluster
+from sklearn import metrics
+from sklearn.cluster import KMeans
 from sklearn.manifold import TSNE
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 #stop_words = list(set(nltk.corpus.stopwords.words('english')))
 NUM_CLUSTERS=2
-
-# def cluster_by_syntax():
-#     return
 
 ## def need to keep the sentence with the id.
 def get_hypernyms(transcript):
@@ -71,19 +74,11 @@ def cluster_by_sentiment(transcript):
     kclusterer = KMeansClusterer(NUM_CLUSTERS, distance=nltk.cluster.util.cosine_distance, repeats=25)
     assigned_clusters = kclusterer.cluster(X, assign_clusters=True)
 
-    kmeans = cluster.KMeans(n_clusters=NUM_CLUSTERS)
-    kmeans.fit(X)
+    kmeans_sentiment = cluster.KMeans(n_clusters=NUM_CLUSTERS)
+    kmeans_sentiment.fit(X)
 
-    labels = kmeans.labels_
-    centroids = kmeans.cluster_centers_
-    #
-    # print ("Cluster id labels for inputted data")
-    # print (labels)
-    # print ("Centroids data")
-    # print (centroids)
-    #
-    # print ("Score (Opposite of the value of X on the K-means objective which is Sum of distances of samples to their closest cluster center):")
-    # print (kmeans.score(X))
+    labels = kmeans_sentiment.labels_
+    centroids = kmeans_sentiment.cluster_centers_
 
     silhouette_score = metrics.silhouette_score(X, labels, metric='euclidean')
     model = TSNE(n_components=2, random_state=0)
@@ -91,12 +86,11 @@ def cluster_by_sentiment(transcript):
     Y=model.fit_transform(X)
     plt.scatter(Y[:, 0], Y[:, 1], c=assigned_clusters, s=290,alpha=.5)
 
-
     for j in range(len(sentences)):
        plt.annotate(assigned_clusters[j],xy=(Y[j][0], Y[j][1]),xytext=(0,0),textcoords='offset points')
        print ("%s %s" % (assigned_clusters[j],  sentences[j]))
 
-    plt.show()
+    return kmeans_sentiment
 
 
 def cluster_by_tfidf(transcript):
@@ -113,6 +107,11 @@ def cluster_by_tfidf(transcript):
 
     return tdidf_kmeans
 
+
+def cluster_by_syntax(transcript):
+    # need to get n-grams
+    # then cluster by n-grams I guess??
+    return
 
 def input_transcript(transcripts_path):
     transcript = {}
@@ -131,135 +130,11 @@ if __name__ == '__main__':
     transcripts_path = './' + args.path + '/' + args.path + '_transcripts_analyzed.json'
     transcript = input_transcript(transcripts_path)
 
-    # kmeans_tdidf = cluster_by_tfidf(transcript)
+    kmeans_tdidf = cluster_by_tfidf(transcript)
     kmeans_sentiment = cluster_by_sentiment(transcript)
-
+    # plt.show()
 
 
 
 ## tfidf special thanks to https://medium.com/@MSalnikov/text-clustering-with-k-means-and-tf-idf-f099bcf95183
-
-
 ## sentiment: https://ai.intelligentonlinetools.com/ml/tag/k-means-clustering-example/
-
-
-
-
-from gensim.models import Word2Vec
-
-from nltk.cluster import KMeansClusterer
-import nltk
-import numpy as np
-
-from sklearn import cluster
-from sklearn import metrics
-
-# training data
-
-# sentences = [['this', 'is', 'the', 'one','good', 'machine', 'learning', 'book'],
-#             ['this', 'is',  'another', 'book'],
-#             ['one', 'more', 'book'],
-#             ['weather', 'rain', 'snow'],
-#             ['yesterday', 'weather', 'snow'],
-#             ['forecast', 'tomorrow', 'rain', 'snow'],
-#             ['this', 'is', 'the', 'new', 'post'],
-#             ['this', 'is', 'about', 'more', 'machine', 'learning', 'post'],
-#             ['and', 'this', 'is', 'the', 'one', 'last', 'post', 'book']]
-
-
-
-# model = Word2Vec(sentences, min_count=1)
-#
-#
-# def sent_vectorizer(sent, model):
-#     sent_vec =[]
-#     numw = 0
-#     for w in sent:
-#         try:
-#             if numw == 0:
-#                 sent_vec = model[w]
-#             else:
-#                 sent_vec = np.add(sent_vec, model[w])
-#             numw+=1
-#         except:
-#             pass
-#
-#     return np.asarray(sent_vec) / numw
-#
-#
-# X=[]
-# for sentence in sentences:
-#     X.append(sent_vectorizer(sentence, model))
-#
-# print ("========================")
-# print (X)
-#
-#
-#
-#
-# # note with some version you would need use this (without wv)
-# #  model[model.vocab]
-# print (model[model.wv.vocab])
-#
-#
-#
-#
-# print (model.similarity('post', 'book'))
-# print (model.most_similar(positive=['machine'], negative=[], topn=2))
-#
-#
-#
-
-#
-# NUM_CLUSTERS=2
-# kclusterer = KMeansClusterer(NUM_CLUSTERS, distance=nltk.cluster.util.cosine_distance, repeats=25)
-# assigned_clusters = kclusterer.cluster(X, assign_clusters=True)
-# print (assigned_clusters)
-
-
-
-# for index, sentence in enumerate(sentences):
-#     print (str(assigned_clusters[index]) + ":" + str(sentence))
-
-
-
-
-# kmeans = cluster.KMeans(n_clusters=NUM_CLUSTERS)
-# kmeans.fit(X)
-#
-# labels = kmeans.labels_
-# centroids = kmeans.cluster_centers_
-#
-# print ("Cluster id labels for inputted data")
-# print (labels)
-# print ("Centroids data")
-# print (centroids)
-#
-# print ("Score (Opposite of the value of X on the K-means objective which is Sum of distances of samples to their closest cluster center):")
-# print (kmeans.score(X))
-#
-# silhouette_score = metrics.silhouette_score(X, labels, metric='euclidean')
-#
-# print ("Silhouette_score: ")
-# print (silhouette_score)
-#
-#
-# import matplotlib.pyplot as plt
-#
-# from sklearn.manifold import TSNE
-#
-# model = TSNE(n_components=2, random_state=0)
-# np.set_printoptions(suppress=True)
-#
-# Y=model.fit_transform(X)
-#
-#
-# plt.scatter(Y[:, 0], Y[:, 1], c=assigned_clusters, s=290,alpha=.5)
-#
-#
-# for j in range(len(sentences)):
-#    plt.annotate(assigned_clusters[j],xy=(Y[j][0], Y[j][1]),xytext=(0,0),textcoords='offset points')
-#    print ("%s %s" % (assigned_clusters[j],  sentences[j]))
-#
-#
-# plt.show()
