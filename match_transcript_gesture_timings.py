015960564281798d5aa3ca9e7d84b57235ed6147
Carolyn Saund <carolyn.saund@gmail.com>
2019-10-21 16:48:03 +0100

    sort gestures by start time and video file
    Former-commit-id: 94d2410ea6bcb8766c2c8316df0f15d2b29bf94c

---
+++ match_transcript_gesture_timings.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+print "importing libs"
+import os
+import io
+import argparse
+import subprocess
+import json
+import argparse
+from google.cloud import storage
+from google.cloud import bigquery
+
+devKey = str(open("/Users/carolynsaund/devKey", "r").read()).strip()
+from apiclient.discovery import build
+service = build('language', 'v1', developerKey=devKey)
+collection = service.documents()
+
+os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/carolynsaund/google-creds.json"
+client = bigquery.Client()
+dataset_id = 'my_dataset'
+
+
+gesture_timings_bucket = "speaker_timings"
+transcript_bucket = "audio_transcript_buckets_1"
+
+
+def read_data(fp):
+    with open(fp, 'r') as f:
+        t = json.load(f)
+        return t
+
+def touch(path):
+    with open(path, 'a'):
+        os.utime(path, None)
+
+def download_blob(bucket_name, source_blob_name, destination_file_name):
+    """Downloads a blob from the bucket."""
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    blob = bucket.blob(source_blob_name)
+    blob.download_to_filename(destination_file_name)
+    print('Blob {} downloaded to {}.'.format(
+        source_blob_name,
+        destination_file_name))
+
+def get_speaker_timings(speaker):
+    print "getting timings for %s" % speaker
+    outfile_path = "tmp.json"
+    touch(outfile_path)
+    download_blob(gesture_timings_bucket, speaker + "_timings.json", outfile_path)
+    timings = read_data(outfile_path)
+    os.remove(outfile_path)
+    return timings
+
+def get_video_transcript(video_name):
+    print "getting transcript for %s" % video_name
+    outfile_path = "tmp.json"
+    touch(outfile_path)
+    download_blob(transcript_bucket, video_name.replace(".mp4", ".json"), outfile_path)
+    transcript = read_data(outfile_path)
+    os.remove(outfile_path)
+    return transcript
+
+def match_transcript_to_timing(timings):
+    phrases = timings['phrases']
+    # will loading all transcripts be too much for memory? oh well!
+
+    # break up timings by transcripts (all videos)
+    # then sort by start time because the transcripts are already sorted by start time.
+    all_videos = list(set([p['phase']['video_fn'] for p in phrases]))
+
+    # avoid loading all the transcripts into memory at once
+    for v in all_videos:
+        transcript = get_video_transcript(video_fn)
+        # break up our dict based on the video transcript.
+        # get all the items from our phrases with this video fn
+        gestures = [x for x in phrases if x['phase']['video_fn'] == v]
+        gs = sorted(gestures, key=sort_start_time)
+        # gs is made up of things that look like this
+        # {u'phase':
+        #   {u'video_fn': u'27._History_of_Rock_-_Rockabilly_Popsters-UYwNGaK4k8U.mp4', u'end_seconds': 477.11044400000003, u'transcript': u'', u'start_seconds': 460.960961},
+        #    u'gestures':
+        #       [{u'end_seconds': 477.11044400000003, u'start_seconds': 460.960961}],
+        #    u'id': 88828}
+
+
+def sort_start_time(x):
+    return x['phase']['start_seconds']
+
+## take gesture timings for a speaker
+## take video transcripts for that speaker
+## matches them up
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=argparse.RawDescriptionHelpFormatter)
+    parser.add_argument('-base_path', '--base_path', help='base folder path of dataset', required=True)
+    parser.add_argument('-speaker', '--speaker', default='optionally, run only on specific speaker', required=False)
+    # --base_path /Users/carolynsaund/github/gest-data/data --speaker rock
+    args = parser.parse_args()
+
+    timings = get_speaker_timings(args.speaker)
