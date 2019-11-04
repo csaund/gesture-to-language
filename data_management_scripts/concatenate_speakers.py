fee5ef8b66ce0e418bde5c0521465447df620651
Carolyn Saund <carolyn.saund@gmail.com>
2019-11-04 10:27:32 +0000

    concat timings

--- data_management_scripts/concatenate_speakers.py
+++ data_management_scripts/concatenate_speakers.py
@@ -3,6 +3,7 @@ import json
 import os
 import argparse
 from common_helpers import *
+from tqdm import tqdm
 
 devKey = str(open("/Users/carolynsaund/devKey", "r").read()).strip()
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/Users/carolynsaund/google-creds.json"
@@ -20,16 +21,19 @@ def concat_timings_transcript_id_keyframes(speakers):
     agd = {}
     agd_phrases = []
     id_keyframes = []
-    for s in speakers:
+    for s in tqdm(speakers):
+        print "Getting data and transcript for %s" % s
         agd_phrases.append(get_data_from_blob(FULL_TRANSCRIPT_BUCKET, "%s_timings_with_transcript.json" % s)['phrases'])
-        id_keyframes.append(get_data_from_blob(AGD_BUCKET), "%s_agd.json" % s)
+        id_keyframes.append(get_data_from_blob(AGD_BUCKET, "%s_agd.json" % s))
     agd = {'phrases': agd_phrases}
 
+    print "Uploading full transcript"
     upload_object(FULL_TRANSCRIPT_BUCKET, agd, "conglomerate_timings_with_transcript.json")
+    print "Uploading full gesture data"
     upload_object(AGD_BUCKET, id_keyframes, "conglomerate_agd.json")
 
 
-def conglomerate_speakers(base_path, speakers):
+def conglomerate_speakers(speakers):
     concat_timings_transcript_id_keyframes(speakers)
 
 
@@ -37,11 +41,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description=__doc__,
         formatter_class=argparse.RawDescriptionHelpFormatter)
-    parser.add_argument('-base_path', '--base_path', help='base folder path of dataset', required=True)
     parser.add_argument('-speakers', '--speakers', default='optionally, run only on specific speaker', nargs='+')
     args = parser.parse_args()
 
     #vid_base_path = args.base_path + '/' + args.speaker + '/videos'
     #transcript_path = args.base_path + '/' + args.speaker + '/transcripts'
-    conglomerate_speakers(args.base_path, args.speakers)
-    print args.speakers
+    conglomerate_speakers(args.speakers)
