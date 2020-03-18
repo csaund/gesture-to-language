286b1638e972bc8a7d865ddeeefc35bf62063185
carolyns <carolyn.saund@gmail.com>
2020-03-18 18:12:20 +0000

    created files for different analyzers

---
+++ data_analysis/SentimentAnalyzer.py
@@ -0,0 +1,23 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+
+# analyzes sentences according to https://github.com/cjhutto/vaderSentiment#python-code-example
+# so returns in format:
+# {
+#   'pos': 0.746,
+#   'compound': 0.8316,
+#   'neu': 0.254,
+#   'neg': 0.0
+# }
+
+class SentimentAnalyzer:
+    def __init__(self):
+        self.analyzer = SentimentIntensityAnalyzer
+
+    # takes a string that represents a single sentence,
+    # returns
+    def analyze_sentence(self, s):
+        score = self.analyzer.polarity_scores(s)
+        return score
+
+    def analyze_sentences(self, s_list):
